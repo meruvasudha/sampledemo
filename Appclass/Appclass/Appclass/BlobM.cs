bf51d2493d23f419980254a25bc4e5ad8fbd206a
meruvasudha <sudhaecpl@gmail.com>
2017-05-03 11:56:19 +0530

    sample
    sample

---
+++ Appclass/Appclass/Appclass/BlobM.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.IO;
+namespace Appclass
+{
+   public class BlobM
+    {
+
+        public static BlobM Instance
+        {
+            get;
+        } = new BlobM();
+
+        public BlobM()
+        {
+
+            _lowrescontainer = _blobclient.GetContainerReference("fulllas");
+            _fullrescontainer = _blobclient.GetContainerReference("lowers");
+
+        }
+
+
+        CloudBlobClient _blobclient = CloudStorageAccount.Parse(connectionString).CreateCloudBlobClient();
+
+
+        const string connectionString = "DefaultEndpointsProtocol=https;AccountName=xamublobdemo;AccountKey=ZdpTzGriWN28xKwK7wj8zWdcFxCZAYvCAVQNGgYLDw7ICLcjD8jc9I7VGtomvY3u3L7nirxXGaop52Os2OviRQ==;";
+
+
+
+        CloudBlobContainer _lowrescontainer;
+        CloudBlobContainer _fullrescontainer;
+
+
+        public async Task<List<Uri>> getallblobasync()
+        {
+            var centoken = new BlobContinuationToken();
+            var alosblobs = await _fullrescontainer.ListBlobsSegmentedAsync(centoken).ConfigureAwait(false);
+            var uris = alosblobs.Results.Select(b => b.Uri).ToList();
+
+            return uris;
+
+        }
+
+        public async Task uploadfileasync(string localpath)
+        {
+            var uniqueblobname = Guid.NewGuid().ToString();
+
+            uniqueblobname += Path.GetExtension(localpath);
+            var blobref = _fullrescontainer.GetBlockBlobReference(uniqueblobname);
+
+            //  await blobref.UploadFromFileAsync(localpath).ConfigurAwait(false);
+
+
+
+
+        }
+
+
+    }
+}
+
+    
+
---
+++ Appclass/Appclass/Appclass/Page1.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Appclass
+{
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            rootlayout.Children.Insert(0, new ScrollView
+            {
+                Content = _wraplayout,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            }
+
+
+                     );
+
+        }
+
+
+        StackLayout _wraplayout = new StackLayout();
+
+
+
+
+        bool isrefreahing;
+
+
+        void showstatus(string msg, bool active = true)
+        {
+            txtstatus.Text = msg;
+            activity.IsVisible = activity.IsRunning = active;
+
+        }
+
+        async void refreshimage(Object o, EventArgs e)
+        {
+            if (isrefreahing)
+            {
+                return;
+            }
+            isrefreahing = true;
+            showstatus("refreshing....", true);
+
+            await updateallimagesasync();
+            showstatus("Done....", false);
+
+
+        }
+
+        async Task updateallimagesasync()
+        {
+            _wraplayout.Children.Clear();
+            var uris = await BlobM.Instance.getallblobasync();
+            foreach (var uri in uris)
+            {
+
+                var img = new Image
+                {
+                    Source = ImageSource.FromUri(uri),
+                    WidthRequest = 70
+
+                };
+                _wraplayout.Children.Add(img);
+            }
+
+
+        }
+        public void addimage(Object o, EventArgs e)
+        {
+
+
+        }
+
+    }
+}
